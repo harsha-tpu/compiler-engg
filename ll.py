25b6990e71b8459655b64ca8fe4064ab70527c75
harsha-tpu <126003105@sastra.ac.in>
2025-02-24 22:53:18 +0530

    somemore were added

--- ll.py
+++ ll.py
@@ -65,7 +65,17 @@ while updated:
                     storage = {symbol}
 
 def first_of_string(symbols):
-    pass
+    if not symbols: 
+        return {'#'}
+    result = set()
+    for symbol in symbols: 
+        sym_first = first_of(symbol)
+        result.update(sym_first - {'#'})
+        if '#' not in sym_first:
+            break
+        else: 
+            result.add('#')
+    return result
 
 # constructing parse table
 parse_table = defaultdict(dict)
@@ -77,3 +87,37 @@ for nt in non_terminals:
         if '#' in first_production:
             for terminal in follow[nt]:
                 parse_table[nt][terminal] = production
+
+# display parse table here
+
+# for string checking 
+string = input("Enter a string to check: ")
+tokens = string.strip().split() + ['$']
+stack = ['$']
+stack.append(non_terminals[0])
+index = 0
+while stack: 
+    cur_stack = "".join(stack[::-1])
+    remain_inp = "".join(tokens[index:])
+    top = stack.pop()
+    if top in  terminals or top == '$':
+        if top == tokens[index]: 
+            # match found 
+            index += 1
+        else: 
+            # not match error
+            print('error')
+            break
+    else: 
+        token = tokens[index]
+        production = parse_table.get(top, {}).get(token)
+        if production is None: 
+            print('error') 
+            break
+        else: 
+            if production != ['#']:
+                for symbol in reversed(production):
+                    stack.append(symbol)
+    if top == '$' and tokens[index] == '$':
+        print('string parsed success') 
+        break      
